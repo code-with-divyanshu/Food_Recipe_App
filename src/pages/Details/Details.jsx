c8c06aa04ba12bf77f8c448f6dc0e6ab7e7a47d4
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-07-12 20:05:58 +0530

    Add Pages

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function Details() {
+  const params = useParams();
+
+  console.log(params);
   return <div>Details</div>;
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../../context";
+import RecipeItem from "../../components/RecipeItem/RecipeItem";
 
 export default function Home() {
-  return <div>Home</div>;
+  const { loading, recipeList } = useContext(GlobalContext);
+
+  if (loading) return <div>Loadin... Please Wait!</div>;
+
+  return (
+    <>
+      <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+        {recipeList && recipeList.length > 0 ? (
+          recipeList.map((item) => <RecipeItem item={item} />)
+        ) : (
+          <div>
+            <p className="lg:tex-4xl text-xl text-center text-black font-extrabold">
+              Nothing To Show, Searh For Recipe Items
+            </p>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
