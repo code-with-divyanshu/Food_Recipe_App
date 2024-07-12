d3a39a16f3e8ce8266e3547bed8c86d04ceddc84
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-07-12 20:07:27 +0530

    Create Context

---
+++ src/context/index.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { createContext } from "react";
+
+export const GlobalContext = createContext(null);
+
+export default function GlobalState({ children }) {
+  const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+      );
+
+      const data = await res.json();
+
+      if (data?.data?.recipes) {
+        setRecipeList(data?.data?.recipes);
+        setLoading(false);
+        setSearchParam("");
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setSearchParam("");
+    }
+  }
+
+  console.log(loading, recipeList);
+
+  return (
+    <GlobalContext.Provider
+      value={{ searchParam, loading, recipeList, setSearchParam, handleSubmit }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+}
